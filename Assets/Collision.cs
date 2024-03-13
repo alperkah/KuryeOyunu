d727a27b4e987ff9848ce99bd0d07f833e1cf150
alperkah <alperkah@hotmail.com>
2024-03-13 14:58:22 +0300

    Collision and Triger
    Collision and Triger added

---
+++ Assets/Collision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Uff!!!");
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Bir geçiþ yaþandý!!!");
+    }
+}
